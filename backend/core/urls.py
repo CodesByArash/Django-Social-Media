bd47453ae1e86af578278c90eee8fe6833dfc781
arash <arashmgn76@gmail.com>
2022-09-30 17:20:32 +0330

    added new feature: post detail view, modified home page

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import home
+from .views import home, post
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('post/<slug:pk>',post, name='post'),
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,9 +1,17 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
+from .models import Post
 # Create your views here.
 
 
 @login_required
 def home(request):
     return render(request, 'socialmedia/index.html')
+
+
+def post(request,pk):
+    post = Post.objects.filter(id=pk)
+    context ={'posts': post}
+    return render(request ,'socialmedia/index.html' ,context=context)
